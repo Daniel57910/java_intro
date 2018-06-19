ed25ce2a133529b83af7650f42df55045801cdf2
Daniel Miller <danielmiller5791@gmail.com>
2018-06-19 14:25:11 +0100

    basic string interpolation completed in java

---
+++ chapter2/exercises/Strings.java
@@ -0,0 +1,9 @@
+public class Strings {
+  public static void main(String[] args) {
+    String Daniel = "Daniel";
+    String Bob = "Bob";
+    System.out.println(Daniel);
+    System.out.println(Bob);
+    System.out.println(Daniel + " and " + Bob + " are learning to code");
+  }
+}
