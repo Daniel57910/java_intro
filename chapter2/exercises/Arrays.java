4ce6289984fcfc55940730bb94aaeb1ce3d7bcdb
Daniel Miller <danielmiller5791@gmail.com>
2018-06-20 17:30:42 +0100

    chapter 2 completed of guide

--- chapter2/exercises/Arrays.java
+++ chapter2/exercises/Arrays.java
@@ -1,10 +1,29 @@
 public class Arrays {
   public static void main(String[] args) {
+    //dynamic memory allocation for array of strings
     String[] stringArray = {"dogs", "cats", "rabbits", "chickens"};
+
+    int[] intArray = {100, 200, 300, 400, 500, 600};
+
+    // static memory allocation for array of integers
+    int[]test = new int[10];
+    for (int i = 0; i < intArray.length; i++) {
+      intArray[i] *= 2;
+    }
+
+    for (int i = 0; i < test.length; i++) {
+      test[i] = i * 2;
+      System.out.println(test[i]);
+    }
 
     for (int i  = 0; i < stringArray.length; i++) {
       System.out.println("Animal = " + stringArray[i]);
     }
+
+    for (int i = 0; i < intArray.length; i++) {
+      System.out.println("Integer doubled  = " + intArray[i]);
+    }
+    
   }
 
 
