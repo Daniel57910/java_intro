4a3bbb79ca2e177a6a563fbd624c579ada04c832
Daniel Miller <danielmiller5791@gmail.com>
2018-06-21 13:16:24 +0100

    looping through array in java completed

--- chapter3/exercises/LoopThroughArray.java
+++ chapter3/exercises/LoopThroughArray.java
@@ -1,3 +1,21 @@
-// can you remember how to set up your main function?
+public class LoopThroughArray {
+  public static void main(String[] args) {
+    int[]intArray = new int[10];
+    makeArr(intArray);
+    printArr(intArray);
+  }
 
-// loop through an integer array and print out each element
+  public static void makeArr(int[] intArray) {
+    for (int i = 0; i < intArray.length; i++) {
+      intArray[i] = i + 1;
+    }
+  }
+
+  public static void printArr(int[] intArray) {
+    for (int i = 0; i < intArray.length; i++) {
+      System.out.println(intArray[i]);
+    }
+  }
+
+}
+
