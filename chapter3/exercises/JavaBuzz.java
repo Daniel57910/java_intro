30a6ed7810b4695fa0be5dd293a4c29958ec2c66
Daniel Miller <danielmiller5791@gmail.com>
2018-06-21 13:34:31 +0100

    java buzz working but need to refactor

--- chapter3/exercises/JavaBuzz.java
+++ chapter3/exercises/JavaBuzz.java
@@ -8,6 +8,17 @@ public class JavaBuzz {
   }
 
   public static void playFizzBuzz(int fizzBuzz) {
-    System.out.println(fizzBuzz);
+    if (fizzBuzz % 15 == 0) {
+      System.out.println("JAVABUZZ");
+    }
+    else if (fizzBuzz % 5 == 0) {
+      System.out.println("BUZZ");
+    }
+    else if (fizzBuzz % 3 == 0) {
+      System.out.println("FIZZ");
+    }
+    else {
+      System.out.println(fizzBuzz);
+    }
   }
 }
